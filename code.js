4d0635bff8e52bb4e051f55df4a73bd4f2b298f2
oniids <jdino.abaya@gmail.com>
2024-08-05 22:08:02 +0800

    Project Done and working

--- code.js
+++ code.js
@@ -1,27 +1,51 @@
 const container = document.querySelector(".container");
+const btn = document.querySelector(".btn");
+const eraseButton = document.querySelector(".btn.eraser");
+const year = document.getElementById('year');
 
-const rows = 16;
-const columns = 16;
+const gridSize = 600;
 
-// for (let i = 0; i < rows; i++) {
-//     const row = document.createElement('div');
-//     row.className = "grid";
-//     container.appendChild(row);
-//     for (let j = 1; j <columns; j++ ){
-//         const column = document.createElement("div");
-//         column.className = "grid";
-//         row.appendChild(column);
-//     }
-// }
+function createGrid(numberofGrid) {
+    container.innerHTML = '';
+    const squareSize = gridSize / numberofGrid;
 
-
-for (let i = 0; i < 16; i++){
-    const div = document.createElement('div');
-    div.className = "grid";
-    container.appendChild(div);
+    for (let i=0; i< numberofGrid * numberofGrid; i++){
+        const div = document.createElement('div');
+        div.classList.add('grid');
+        div.style.width = `${squareSize}px`;
+        div.style.height = `${squareSize}px`;
+        div.addEventListener('mouseenter', (e) => {
+            div.style.backgroundColor = "rgb("+e.offsetX+","+e.offsetY+",60)";
+            if (e.altKey){
+                div.style.backgroundColor = "white"; 
+            }
+        });
+        container.appendChild(div);
+    }
 }
 
-function hover(e){
-    console.log(e.target);
+
+function erase(e){
+    const box1 = document.getElementsByClassName('grid');
+    for (let i = 0; i < box1.length; i++){
+        box1[i].style.backgroundColor = "white";
+        };
 }
 
+
+function addGrid(e) {
+    let numberofGrid = Number(prompt("How many grids do you want? Max 100"));
+
+    if (isNaN(numberofGrid) || numberofGrid <=0 || numberofGrid > 100){
+        alert("Please enter a a number from 1 to 100");
+    } else{
+        createGrid(numberofGrid);
+    }
+}
+
+eraseButton.addEventListener('click', erase);
+btn.addEventListener('click', addGrid);
+year.innerHTML = new Date().getFullYear();
+
+// Initilize grid
+createGrid(16);
